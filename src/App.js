c2d976d2832382822e181e6c1f62ac083550b1d2
Gurlabe <Gurlabe72@gmail.com>
2019-06-02 21:50:40 -0700

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-Hello World
+    <div className="wrapper">
+      <div className="form-validation">
+        <h1>Create Account</h1>
+        <form onSubmit={this.handleSubmit} noValidate>
+          <div className="firstName">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" className="" placeholder="First Name Here" name="firstName" noValidate onChange={this.handleChange}/>
+
+          </div>
+        </form> 
+      </div>
     </div>
   );
+}
 }
 
 export default App;
