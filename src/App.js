e5ed3fca6a03ddb28a696902072329af3a8696e3
Gurlabe <Gurlabe72@gmail.com>
2019-06-02 23:58:19 -0700

    creating sign in page with added css styling

--- src/App.js
+++ src/App.js
@@ -10,8 +10,43 @@ class App extends Component {
         <form onSubmit={this.handleSubmit} noValidate>
           <div className="firstName">
             <label htmlFor="firstName">First Name</label>
-            <input type="text" className="" placeholder="First Name Here" name="firstName" noValidate onChange={this.handleChange}/>
-
+            <input 
+                type="text" 
+                className="" 
+                placeholder="First Name Here" 
+                name="firstName" 
+                noValidate 
+                onChange={this.handleChange}/>
+          </div>
+          <div className="lastName">
+            <label htmlFor="lastName">Last Name</label>
+            <input 
+                type="text" 
+                className="" 
+                placeholder="Last Name Here" 
+                name="lastName" 
+                noValidate 
+                onChange={this.handleChange}/>
+          </div>
+          <div className="email">
+            <label htmlFor="email">Email</label>
+            <input 
+                type="text" 
+                className="" 
+                placeholder="Email Here" 
+                name="email" 
+                noValidate 
+                onChange={this.handleChange}/>
+          </div>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input 
+                type="text" 
+                className="" 
+                placeholder="Password Here" 
+                name="password" 
+                noValidate 
+                onChange={this.handleChange}/>
           </div>
         </form> 
       </div>
